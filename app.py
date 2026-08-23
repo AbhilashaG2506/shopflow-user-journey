6ee58668bbba4e8f1f96baab612a57f059d2c81a
ankita <ankita@gmail.com>
2026-08-24 03:09:50 +0530

    Fix app indentation

--- app.py
+++ app.py
@@ -2299,7 +2299,7 @@ if __name__ == "__main__":
     print("==============================================")
     print()
 
-   if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(
         debug=True,
         host="0.0.0.0",
